632f7cd64c2017b123a285976d1db59b7e96cbf0
maikolfox <ahoueromeo@gmail.com>
2020-01-18 16:41:13 +0000

    ..

--- src/components/asset/ConfigUrl.jsx
+++ src/components/asset/ConfigUrl.jsx
@@ -1,7 +1,7 @@
 const ConfigUrl={
 
-basePath:"http://localhost:8089/api/effop"
-//basePath:"http://localhost:3553/api"
+  basePath:"http://localhost:8089/api/effop"
+//basePath:"http://localhost:8089/api/effop"
 
 
 }
